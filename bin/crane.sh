7778b0a54fed7a55399570088fc691e2e7090dd9
Calvin Gerling <calvin.gerling@singlebit.org>
2020-06-25 09:15:25 -0300

    refactor(crane): use function clojure to simulate private functions
    Avoid script functions leaking to other scripts making it harder to detect where they come from

--- bin/crane.sh
+++ bin/crane.sh
@@ -4,28 +4,32 @@ export SCRIPTS_DIR=$(dirname $(realpath "$0"))
 export ROOT_DIR=$(realpath "$SCRIPTS_DIR/..")
 export IMAGES_DIR="$ROOT_DIR/images"
 
-function find_cmd_file {
-  local commands_dir="$SCRIPTS_DIR/commands"
-  local cmd_name=$1
-
-  case "$cmd_name" in
-    'build') echo "$commands_dir/$cmd_name.sh";;
-    *) echo "";;
-  esac
-}
-
 function crane_cmd {
-  local cmd=$1
-  local cmd_file=$(find_cmd_file $cmd)
+  function find_cmd_file {
+    local commands_dir="$SCRIPTS_DIR/commands"
+    local cmd_name=$1
 
-  local cmd_args=${@:2}
+    case "$cmd_name" in
+      'build') echo "$commands_dir/$cmd_name.sh";;
+      *) echo "";;
+    esac
+  }
 
-  if [ -n "$cmd_file" ] && [ -x "$cmd_file" ]; then
-    exec "$cmd_file" "$cmd_args"
-  else
-    echo "crane: '$cmd' is not a command."
-    echo "Use 'crane --help' to see all commands."
-  fi
+  function run_cmd {
+    local cmd=$1
+    local cmd_file=$(find_cmd_file $cmd)
+
+    local cmd_args=${@:2}
+
+    if [ -n "$cmd_file" ] && [ -x "$cmd_file" ]; then
+      exec "$cmd_file" "$cmd_args"
+    else
+      echo "crane: '$cmd' is not a command."
+      echo "Use 'crane --help' to see all commands."
+    fi
+  }
+
+  run_cmd $@
 }
 
 crane_cmd $@
