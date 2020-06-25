f881758e58021d689ed4b3f857cfe7202e609ed9
Calvin Gerling <calvin.gerling@singlebit.org>
2020-06-25 20:13:39 -0300

    refactor(crane): unwrap build command function
    When commands have private functions the command body must be wrapped in a function to avoid mixing logic with function declaration, in this case the build command does not have any private function therfore does not need to be wrapped

--- bin/commands/build.sh
+++ bin/commands/build.sh
@@ -4,25 +4,21 @@ source "$SCRIPTS_DIR/utils/utils.sh"
 source "$SCRIPTS_DIR/utils/docker.sh"
 
 function build_cmd {
-  function run_cmd {
-    local image_expr=$1
+  local image_expr=$1
 
-    IFS=' ' read -r image_name image_version <<< "$(parse_image_expr $image_expr)"
+  IFS=' ' read -r image_name image_version <<< "$(parse_image_expr $image_expr)"
 
-    load_image_metadata $IMAGES_DIR $image_name
+  load_image_metadata $IMAGES_DIR $image_name
 
-    image_version=$(get_image_version $image_version $CURRENT_VERSION)
+  image_version=$(get_image_version $image_version $CURRENT_VERSION)
 
-    local registry="docker.pkg.github.com"
-    local owner="cgerling/dockeryard"
-    image_tag=$(build_image_tag $image_name $image_version $registry $owner)
+  local registry="docker.pkg.github.com"
+  local owner="cgerling/dockeryard"
+  local image_tag=$(build_image_tag $image_name $image_version $registry $owner)
 
-    local image_path="$IMAGES_DIR/$image_name"
-    local build_options=${@:2}
-    eval "$(build_docker_build_cmd $image_path $image_tag "$build_options")"
-  }
-
-  run_cmd $@
+  local image_path="$IMAGES_DIR/$image_name"
+  local build_options=${@:2}
+  eval "$(build_docker_build_cmd $image_path $image_tag "$build_options")"
 }
 
 build_cmd $@
