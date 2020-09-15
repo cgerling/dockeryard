f247851bae8d4375301c8aad3b7a6ef920e04ca8
Calvin Gerling <calvin.gerling@singlebit.org>
2020-09-15 19:47:14 -0300

    feat(crane): create functions to retrieve user information
    Add support for the environment variables  and  in order to namespace the Docker Image

--- bin/utils/utils.sh
+++ bin/utils/utils.sh
@@ -1,5 +1,27 @@
 #!/usr/bin/env bash
 
+function get_registry {
+  local DEFAULT_REGISTRY="docker.pkg.github.com"
+
+  local registry=$CRANE_REGISTRY
+  if [[ -z "$registry" ]]; then
+    registry=$DEFAULT_REGISTRY
+  fi
+
+  echo "$registry"
+}
+
+function get_owner {
+  local DEFAULT_OWNER="cgerling/dockeryard"
+
+  local owner=$CRANE_OWNER
+  if [[ -z "$owner" ]]; then
+    owner=$DEFAULT_OWNER
+  fi
+
+  echo "$owner"
+}
+
 function load_image_metadata {
   local images_dir=$1
   local image_name=$2
